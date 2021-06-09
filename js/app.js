dce48bec9b817229acdfcad466b5208ab6776f61
adhammhaydat <adhammohidat123@gmail.com>
2021-06-09 07:19:49 -0700

    fixed function with tawfeeq

--- js/app.js
+++ js/app.js
@@ -7,56 +7,104 @@ if (userAnsweraboutme) {alert('Lets see about that!');
 }
 else {alert('Lets play a game!');
 }
-
 let userScore = 0;
-let smokeQuestion = prompt('do i smoke?').toLowerCase();
-if (smokeQuestion === 'yes' || smokeQuestion === 'y' ){
-  alert('WHATT?? I NEVER SMOKE!!');
-} else if (smokeQuestion === 'no' || smokeQuestion === 'n' ){
-  alert('Correct!!');
-  console.log('Question1: True');
-  userScore = userScore + 1;
-} else {alert('WRONG! please enter a valid answer!');
-}
 
-let mastersQuestion = prompt('did i take a masters degree?').toLowerCase();
-if (mastersQuestion === 'yes' || mastersQuestion === 'y' ){
-  alert('MAN I THINK YOU REALLY KNOW ME!!');
-  console.log('Question2: True');
-  userScore = userScore + 1;
-} else if (mastersQuestion === 'no' || mastersQuestion === 'n' ){
-  alert('REALLY?? i have actually 2 not 0!!');
-} else {alert('WRONG! please enter a valid answer!');
-}
+function userAnswer(question,correct,wrong){
+  let userQuestion = prompt(question).toLowerCase();
+  while (
+    userQuestion !== 'yes' &&
+    userQuestion !== 'y' &&
+    userQuestion !== 'no' &&
+    userQuestion !== 'n'
+  ) {
+    alert('wrong Answer');
+    userQuestion = prompt(question).toLowerCase();
+  }
 
-let catsQuestion = prompt('do i love cats?').toLowerCase();
-if (catsQuestion === 'yes' || catsQuestion === 'y' ){
-  alert('MAN YOU ARE DOING THIS!!');
-  console.log('Question3: True');
-  userScore = userScore + 1;
-} else if (catsQuestion === 'no' || catsQuestion === 'n' ){
-  alert('who doesnt love cats!!');
-} else {alert('WRONG! please enter a valid answer!');
+  if (userQuestion === 'yes' || userQuestion === 'y') {
+    userQuestion = alert(correct);
+    userScore++;
+  } else {
+    userQuestion = alert(wrong);
+  }
 }
+userAnswer(
+  'do i smoke?',
+  'WHATT?? I NEVER SMOKE!!',
+  'Correct!!'
+);
+userAnswer(
+  'did i take a masters degree?',
+  'MAN I THINK YOU REALLY KNOW ME!!',
+  'REALLY?? i have actually 2 not 0!!'
+);
+userAnswer(
+  'do i love cats?',
+  'MAN YOU ARE DOING THIS!!',
+  'Man please get to know me better!'
+);userAnswer(
+  'can i learn programming?',
+  'MAN YOU ARE GENIUS!!',
+  'GREAT JOB!!'
+);userAnswer(
+  'did i study design?',
+  'MAN WE HAVE TO MEET NOW!!',
+  'Man please get to know me better!'
+);
 
-let programmingQuestion = prompt('can i learn programming?').toLowerCase();
-if (programmingQuestion === 'yes' || programmingQuestion === 'y' ){
-  alert('MAN YOU ARE GENIUS!!');
-  console.log('Question4: True');
-  userScore = userScore + 1;
-} else if (programmingQuestion === 'no' || programmingQuestion === 'n' ){
-  alert('Man why you dont believe in me');
-} else {alert('WRONG! please enter a valid answer!');
-}
-let designQuestion = prompt('did i study design?').toLowerCase();
-if (designQuestion === 'yes' || designQuestion === 'y' ){
-  alert('MAN WE HAVE TO MEET NOW!!');
-  console.log('Question5: True');
-  userScore = userScore + 1;
-} else if (designQuestion === 'no' || designQuestion === 'n' ){
-  alert('Man please get to know me better!');
-} else {alert('WRONG! please enter a valid answer!');
-}
+
+
+
+
+//let userScore = 0;
+// let smokeQuestion = prompt('do i smoke?').toLowerCase();
+// if (smokeQuestion === 'yes' || smokeQuestion === 'y' ){
+//   alert('WHATT?? I NEVER SMOKE!!');
+// } else if (smokeQuestion === 'no' || smokeQuestion === 'n' ){
+//   alert('Correct!!');
+//   console.log('Question1: True');
+//   userScore = userScore + 1;
+// } else {alert('WRONG! please enter a valid answer!');
+// }
+
+// let mastersQuestion = prompt('did i take a masters degree?').toLowerCase();
+// if (mastersQuestion === 'yes' || mastersQuestion === 'y' ){
+//   alert('MAN I THINK YOU REALLY KNOW ME!!');
+//   console.log('Question2: True');
+//   userScore = userScore + 1;
+// } else if (mastersQuestion === 'no' || mastersQuestion === 'n' ){
+//   alert('REALLY?? i have actually 2 not 0!!');
+// } else {alert('WRONG! please enter a valid answer!');
+// }
+
+// let catsQuestion = prompt('do i love cats?').toLowerCase();
+// if (catsQuestion === 'yes' || catsQuestion === 'y' ){
+//   alert('MAN YOU ARE DOING THIS!!');
+//   console.log('Question3: True');
+//   userScore = userScore + 1;
+// } else if (catsQuestion === 'no' || catsQuestion === 'n' ){
+//   alert('who doesnt love cats!!');
+// } else {alert('WRONG! please enter a valid answer!');
+// }
+
+// let programmingQuestion = prompt('can i learn programming?').toLowerCase();
+// if (programmingQuestion === 'yes' || programmingQuestion === 'y' ){
+//   alert('MAN YOU ARE GENIUS!!');
+//   console.log('Question4: True');
+//   userScore = userScore + 1;
+// } else if (programmingQuestion === 'no' || programmingQuestion === 'n' ){
+//   alert('Man why you dont believe in me');
+// } else {alert('WRONG! please enter a valid answer!');
+// }
+// let designQuestion = prompt('did i study design?').toLowerCase();
+// if (designQuestion === 'yes' || designQuestion === 'y' ){
+//   alert('MAN WE HAVE TO MEET NOW!!');
+//   console.log('Question5: True');
+//   userScore = userScore + 1;
+// } else if (designQuestion === 'no' || designQuestion === 'n' ){
+//   alert('Man please get to know me better!');
+// } else {alert('WRONG! please enter a valid answer!');
+// }
 
 let userGuessuni = prompt('how old was i when i worked at SAE?, you have 4 attempts!');
 for ( let i=1; i < 4; i++){
